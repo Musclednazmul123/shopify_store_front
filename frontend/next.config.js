0e2fd2edf14adb83e682cad821a0d572a6c62745
Musclednazmul123 <nazmul.muscled@gmail.com>
2023-06-30 08:00:05 -0400

    car page error fixed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.shopify.com'],
+    domains: ['cdn.shopify.com', "drive.google.com"],
   },
   env: {
     NEXT_PUBLIC_STORE_API: process.env.NEXT_PUBLIC_STORE_API,
